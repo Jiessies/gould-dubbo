172b6002fcdffe1b8f533d45138fb65176754381
huangmingjie <huangmingjie@yupiao.com>
2018-01-17 17:07:58 +0800

    dubbo增加group

--- src/main/java/com/ykly/controller/GouldController.java
+++ src/main/java/com/ykly/controller/GouldController.java
@@ -4,6 +4,7 @@ import com.ykly.service.GouldService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +18,23 @@ public class GouldController {
     private static final Logger logger = LoggerFactory.getLogger(GouldController.class);
     
     @Autowired
+    @Qualifier(value = "gouldService")
     private GouldService gouldService;
+    
+    @Autowired
+    @Qualifier(value = "gouldService2")
+    private GouldService gouldService2;
     
     @GetMapping("/hello")
     public String index() {
         String hello = gouldService.getString();
+        logger.info(hello);
+        return hello;
+    }
+    
+    @GetMapping("/hello2")
+    public String index2() {
+        String hello = gouldService2.getString();
         logger.info(hello);
         return hello;
     }
